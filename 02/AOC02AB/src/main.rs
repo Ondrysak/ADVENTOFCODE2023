5dd829ca30529e41fa952ae94ccabafe73248fa5
Ondrej Nanka <onanka@purestorage.com>
2023-12-03 02:21:39 +0100

    day 2 part B

--- 02/AOC02AB/src/main.rs
+++ 02/AOC02AB/src/main.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 
-fn line_possible(text: &str, r: i32, g: i32, b: i32) -> i32 {
+fn line_possible(text: &str, r: i32, g: i32, b: i32) -> (i32, i32) {
     //Game (\d+): (.*)
     let re_id = Regex::new(r"(\d+): (.*)").unwrap();
     //(\d+) [a-z]+
@@ -12,6 +12,11 @@ fn line_possible(text: &str, r: i32, g: i32, b: i32) -> i32 {
 
     println!("{}",id);
     println!("{}",draws);
+    let mut minred = 0;
+    let mut mingreen = 0;
+    let mut minblue = 0;
+    let mut impossible = false;
+
     for line in draws.split(";") {
         let mut lred = 0;
         let mut lgreen = 0;
@@ -20,23 +25,44 @@ fn line_possible(text: &str, r: i32, g: i32, b: i32) -> i32 {
             let count_int = count.parse::<i32>().unwrap();
 
             match color {
-                "red" => lred = count_int,
-                "green" => lgreen = count_int,
-                "blue" => lblue = count_int,
+                "red" => {
+                    lred = count_int;
+                    if minred < lred {
+                        minred = lred;
+                    }
+                },
+                "green" => {
+                    lgreen = count_int;
+                    if mingreen < lgreen {
+                        mingreen = lgreen;
+                    }
+                },
+                "blue" => {
+                    lblue = count_int;
+                    if minblue < lblue {
+                        minblue = lblue;
+                    }
+                }
                 _ => (),
             };
+
             println!("{}",color);
             println!("{}",count);
         }
         if lred > r || lblue > b || lgreen > g {
             println!("Impossibru yo! Because of {}", line);
-            return 0;
+            impossible = true;
         }
         println!("Found in {}",line);
     }
+    if !impossible {
+        let id_int: i32 = id.parse::<i32>().unwrap();
+        return (id_int,minred*mingreen*minblue);
+    }
+    else{
+        return (0,minred*mingreen*minblue);
+    }
 
-    let id_int: i32 = id.parse::<i32>().unwrap();
-    return id_int;
 }
 
 
@@ -47,9 +73,13 @@ fn main() {
     let green_limit: i32 = 13;
     let blue_limit: i32 = 14;
     let mut sum = 0;
+    let mut powersum = 0;
     for line in input.split("\n") {
         println!("{}",line);
-        sum += line_possible(line,red_limit, green_limit, blue_limit);
+        let (possible, power) = line_possible(line,red_limit, green_limit, blue_limit);
+        sum += possible;
+        powersum += power;
     }
-    println!("Final answer {}", sum);
+    println!("Final answer(A) {}", sum);
+    println!("Final answer(B) {}", powersum);
 }
